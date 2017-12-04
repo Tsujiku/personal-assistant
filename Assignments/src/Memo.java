5e53050d9e972ec874e0f91e7c086b345555579e
chanyipk <chanyipk@naver.com>
2017-12-04 20:10:14 +0900

    Memo modify update

--- Assignments/src/Memo.java
+++ Assignments/src/Memo.java
@@ -8,17 +8,16 @@ public class Memo {
 	public static BufferedWriter write;
 	public static BufferedReader read;
 	public static File file;
+	public static int flag = 0;
 	public static List<String> MemoList;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		// memo class
 
 		int menuSelection;
 		boolean exitMemo = false;
 		int index;
-		
-		
 		System.out.println("<Start Memo>");
 		// file open
 
@@ -32,9 +31,9 @@ public class Memo {
 				String s;
 				int count = 1;
 				while ((s = read.readLine()) != null) {
-					//System.out.println(s);
-					MemoList.add(s);//배열리스트에 저장
-					System.out.println("index : " + count + "||" + MemoList.get(count-1).toString());
+					// System.out.println(s);
+					MemoList.add(s);// 배열리스트에 저장
+					System.out.println("index : " + count + "||" + MemoList.get(count - 1).toString());
 					count++;
 				}
 				read.close();
@@ -42,9 +41,9 @@ public class Memo {
 
 			else { // 파일 새로 생성하기
 				System.out.println("not imformation");
-				
+
 			}
-			//메모리스트 화면에 뿌려주기
+			// 메모리스트 화면에 뿌려주기
 			System.out.println("-------Memo menu-------");
 			System.out.println("1. Insert");
 			System.out.println("2. Modify");
@@ -52,7 +51,7 @@ public class Memo {
 			System.out.println("4. Exit Memo");
 			System.out.println("-----------------------");
 			System.out.print("Press 1/2/3/4: ");
-			
+
 			Scanner scan = new Scanner(System.in);
 			menuSelection = scan.nextInt();
 
@@ -86,39 +85,73 @@ public class Memo {
 
 	}
 
-	public static void Insert() throws IOException{
+	public static void Insert() throws IOException {
 		System.out.print("write the contents : ");
 		Scanner scan = new Scanner(System.in);
 		String contents = scan.next();
 		MemoList.add(contents);
-		confirm();
+	
+		System.out.println("Do you want to save?(y/n)");
+		Scanner issave = new Scanner(System.in);
+		String saveStr = issave.nextLine();
 
-	}
-
-	public static void Modify(int index) {
-		System.out.println(index);
-	}
-
-	public static void Delete(int index) throws IOException{
-		MemoList.remove(index-1);
-		confirm();
-	}
-	public static void confirm() throws IOException{
-		
-		write = new BufferedWriter(new FileWriter(file));
-		
-		//방법1, 이 경우 FileWriter(file, true)로 해야한다.
-		/*int count = MemoList.size()-1;
-		write.write(MemoList.get(count).toString());
-		write.newLine();*/
-		//방법2
-		String allcontents="";
-		for(int count=0;count<MemoList.size();count++) {
-			
-			allcontents += MemoList.get(count).toString() +"\r\n";
-
+		if (saveStr.equals("y")) {
+			confirm();
+		} else {
+			cancel();
 		}
+
+	}
+
+	public static void Modify(int index) throws IOException{
+		System.out.print("write modified contents : ");
+		Scanner scan = new Scanner(System.in);
+		String contents = scan.next();
+		MemoList.remove(index-1);
+		MemoList.add(index-1,contents);
+		
+		System.out.println("Do you want to save?(y/n)");
+		Scanner issave = new Scanner(System.in);
+		String saveStr = issave.nextLine();
+
+		if (saveStr.equals("y")) {
+			confirm();
+		} else {
+			cancel();
+		}
+	}
+
+	public static void Delete(int index) throws IOException {
+		MemoList.remove(index - 1);
+		// 확인할건지 취소할건지 물어보고 confirm, cancel 함수 호출
+		System.out.println("Do you want to save?(y/n)");
+		Scanner issave = new Scanner(System.in);
+		String saveStr = issave.nextLine();
+
+		if (saveStr.equals("y")) {
+			confirm();
+		} else {
+			cancel();
+		}
+
+	}
+
+	public static void confirm() throws IOException {
+
+		write = new BufferedWriter(new FileWriter(file));
+
+		String allcontents = "";
+
+		for (int count = 0; count < MemoList.size(); count++) {
+			allcontents += MemoList.get(count).toString() + "\r\n";
+		}
+
 		write.write(allcontents);
 		write.close();
+	}
+
+	public static int cancel() {
+		// 호출한 곳으로 돌아가기
+		return 0;
 	}
 }

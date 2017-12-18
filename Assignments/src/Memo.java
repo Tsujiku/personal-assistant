b96b596e9fa3fa448cb3493e43923861be89ad35
chanyipk <chanyipk@naver.com>
2017-12-18 14:00:43 +0900

    Modify Memo.java and add MemoTest.java

--- Assignments/src/Memo.java
+++ Assignments/src/Memo.java
@@ -4,21 +4,15 @@ import java.util.List;
 import java.io.*;
 
 public class Memo {
-	static String FILENAME; // static으로 고정
-	public static BufferedWriter write;
-	public static BufferedReader read;
-	public static File file;
-	public static int flag = 0;
-	public static List<String> MemoList;
-
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		// memo class
-
+		File file;
+		List<String> MemoList;
 		int menuSelection;
 		boolean exitMemo = false;
 		int index;
-		System.out.println("<Start Memo>");
+		System.out.println("\n<Start Memo>");
 		// file open
 
 		do {
@@ -27,13 +21,17 @@ public class Memo {
 			boolean isExists = file.exists();
 			if (isExists) { // 파일 존재할 때 내용 읽어오기
 
-				read = new BufferedReader(new FileReader(file));
+				BufferedReader read = new BufferedReader(new FileReader(file));
 				String s;
 				int count = 1;
+				System.out.println("_________________________");
+				System.out.println("index\t | contents");
+				
+				
 				while ((s = read.readLine()) != null) {
 					// System.out.println(s);
 					MemoList.add(s);// 배열리스트에 저장
-					System.out.println("index : " + count + "||" + MemoList.get(count - 1).toString());
+					System.out.println(count + "\t | " + MemoList.get(count - 1).toString());
 					count++;
 				}
 				read.close();
@@ -44,7 +42,8 @@ public class Memo {
 
 			}
 			// 메모리스트 화면에 뿌려주기
-			System.out.println("-------Memo menu-------");
+			System.out.println("_________________________");
+			System.out.println("\n-------Memo menu-------");
 			System.out.println("1. Insert");
 			System.out.println("2. Modify");
 			System.out.println("3. Delete");
@@ -57,18 +56,24 @@ public class Memo {
 
 			switch (menuSelection) {
 			case 1:
-				Insert();
+				System.out.print("write the contents : ");
+				Scanner scanContents = new Scanner(System.in);
+				String contents = scanContents.next();
+				MemoList = Insert(MemoList,contents);
+				isSave(MemoList,file);
 				break;
 
 			case 2:
 				System.out.print("Enter index to Modify : ");
 				index = scan.nextInt();
-				Modify(index);
+				MemoList = Modify(MemoList,index);
+				isSave(MemoList,file);
 				break;
 			case 3:
 				System.out.print("Enter index to Delete : ");
 				index = scan.nextInt();
-				Delete(index);
+				MemoList = Delete(MemoList,index);
+				isSave(MemoList,file);
 				break;
 			case 4:
 				exitMemo = true;
@@ -85,73 +90,63 @@ public class Memo {
 
 	}
 
-	public static void Insert() throws IOException {
-		System.out.print("write the contents : ");
-		Scanner scan = new Scanner(System.in);
-		String contents = scan.next();
+	public static List<String> Insert(List<String> MemoList,String contents){
+		
 		MemoList.add(contents);
-	
-		System.out.println("Do you want to save?(y/n)");
-		Scanner issave = new Scanner(System.in);
-		String saveStr = issave.nextLine();
-
-		if (saveStr.equals("y")) {
-			confirm();
-		} else {
-			cancel();
-		}
-
+		
+		return MemoList;
 	}
 
-	public static void Modify(int index) throws IOException{
+	public static List<String> Modify(List<String> MemoList,int index){
 		System.out.print("write modified contents : ");
 		Scanner scan = new Scanner(System.in);
 		String contents = scan.next();
 		MemoList.remove(index-1);
 		MemoList.add(index-1,contents);
 		
-		System.out.println("Do you want to save?(y/n)");
-		Scanner issave = new Scanner(System.in);
-		String saveStr = issave.nextLine();
-
-		if (saveStr.equals("y")) {
-			confirm();
-		} else {
-			cancel();
-		}
+		return MemoList;
 	}
 
-	public static void Delete(int index) throws IOException {
+	public static List<String> Delete(List<String> MemoList,int index){
 		MemoList.remove(index - 1);
-		// 확인할건지 취소할건지 물어보고 confirm, cancel 함수 호출
+		return MemoList;
+	}
+
+	public static void isSave(List<String> MemoList, File file) throws IOException {
 		System.out.println("Do you want to save?(y/n)");
 		Scanner issave = new Scanner(System.in);
 		String saveStr = issave.nextLine();
-
+		
 		if (saveStr.equals("y")) {
-			confirm();
+			confirm(MemoList, file);
 		} else {
 			cancel();
 		}
-
+	
 	}
+	
+	public static long confirm(List<String> MemoList,File file) throws IOException {
 
-	public static void confirm() throws IOException {
-
-		write = new BufferedWriter(new FileWriter(file));
-
+		BufferedWriter write = new BufferedWriter(new FileWriter(file));
+		long fileSize;
+		
 		String allcontents = "";
 
 		for (int count = 0; count < MemoList.size(); count++) {
 			allcontents += MemoList.get(count).toString() + "\r\n";
 		}
-
+		
 		write.write(allcontents);
 		write.close();
+		
+		fileSize = file.length();
+		return fileSize;
 	}
 
 	public static int cancel() {
-		// 호출한 곳으로 돌아가기
+
 		return 0;
 	}
+	
+	
 }
---
+++ Assignments/src/MemoTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+public class MemoTest {
+	@Test
+	public void testInsert(){
+		Memo memo = new Memo();
+		List<String> MemoList = new ArrayList<String>();
+		String testStr = "testContents";
+		assertTrue(MemoList.size() - memo.Insert(MemoList,testStr).size() == -1);
+	}
+	@Test
+	public void testDelete(){
+		Memo memo = new Memo();
+		List<String> MemoList = new ArrayList<String>();
+		MemoList.add("testContets");
+		assertTrue(MemoList.size() - memo.Delete(MemoList,1).size() == 1);
+	}
+/*	@Test
+	public void testModify() {
+		Memo memo = new Memo();
+		List<String> MemoList = new ArrayList<String>();
+		
+	}*/
+
+	@Test
+	public void testConfirm() throws IOException{
+		Memo memo = new Memo();
+		File file = new File("test.txt");
+		List<String> MemoList = new ArrayList<String>();
+		long beforefileSize = file.length();
+		long afterfileSize;
+		
+		MemoList.add("testContets");
+		afterfileSize = memo.confirm(MemoList,file);
+		file.delete();
+		
+		assertNotEquals(beforefileSize,afterfileSize);
+	}
+
+}
